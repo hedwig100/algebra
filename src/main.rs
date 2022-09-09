48c9179501ce9093ecd56262f96193c5ab3b1231
hedwig100 <pentagon7951@outlook.jp>
2022-09-09 17:04:04 +0900

    [add] cli

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
-use algebra::algebra::fp;
-use algebra::polynomial::monomial::Monomial;
-use algebra::polynomial::poly::Polynomial;
+use algebra::{algebra::fp, buchberger::grobner, equation_parser::parser, polynomial::poly};
+use clap::Parser;
 
-type Fp = fp::Fp<5>;
+const P: i32 = 7;
+const N: usize = 3;
+
+#[derive(Parser)]
+struct Args {
+    #[clap(short = 'e', long = "equation")]
+    equation: String,
+}
 
 fn main() {
-    let x = Polynomial::new(vec![
-        Monomial::new(Fp::new(3), [4, 1, 3]),
-        Monomial::new(Fp::new(3), [1, 2, 3]),
-    ]);
-    println!("{}", x);
+    let args = Args::parse();
+    let polys: Vec<poly::Polynomial<fp::Fp<P>, N>> = match parser::parse(&args.equation) {
+        Ok(polys) => polys,
+        Err(msg) => panic!("{}", msg),
+    };
+    println!(
+        "Original Ideal: ({})",
+        polys
+            .iter()
+            .map(|x| format!("{}", x))
+            .collect::<Vec<String>>()
+            .join(", ")
+    );
+    let grobner = grobner::buchberger(polys);
+    let minimal = grobner::minimal_grobner(grobner);
+    let simple = grobner::simplified_grobner(minimal);
+    println!(
+        "Simplified Grobner Basis: {}",
+        simple
+            .iter()
+            .map(|x| format!("{}", x))
+            .collect::<Vec<String>>()
+            .join(", ")
+    );
 }
